da9265dc71cd6a4cfe583643b1e27bc33b24bb74
tshemsedinov <timur.shemsedinov@gmail.com>
2018-10-16 02:12:52 +0300

    Add examples

--- JavaScript/1-simple.js
+++ JavaScript/1-simple.js
@@ -6,6 +6,8 @@ console.log();
 console.log(`name = '${name}'`);
 
 console.log();
+console.log(`typeof name = '${typeof name}'`);
+console.log(`name.length = ${name.length}`);
 console.log(`name[3] = '${name[3]}'`);
 console.log(`name.charAt(5) = '${name.charAt(5)}'`);
 console.log(`name.charCodeAt(7) = ${name.charCodeAt(7)}`);
--- JavaScript/5-sub.js
+++ JavaScript/5-sub.js
@@ -14,3 +14,9 @@ console.log(`name.substr(-3, 2) = '${name.substr(-3, 2)}'`);
 console.log();
 console.log(`name.substring(2) = '${name.substring(2)}'`);
 console.log(`name.substring(2, 4) = '${name.substring(2, 4)}'`);
+
+console.log();
+console.log(`name.slice(7) = '${name.slice(7)}'`);
+console.log(`name.slice(7, 9) = '${name.slice(7, 9)}'`);
+console.log(`name.slice(-5) = '${name.slice(-5)}'`);
+console.log(`name.slice(-5, -1) = '${name.slice(-5, -1)}'`);
